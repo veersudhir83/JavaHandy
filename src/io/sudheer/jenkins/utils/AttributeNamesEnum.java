a8345842dad9c44fd83b8150895e27b2d098e531
Sudheer Veeravalli <veersudhir83@gmail.com>
2018-04-26 15:57:44 +0530

    Added Code to test enums

---
+++ src/io/sudheer/jenkins/utils/AttributeNamesEnum.java
@@ -0,0 +1,6 @@
+package io.sudheer.jenkins.utils;
+
+public enum AttributeNamesEnum {
+	META_KEYWORD, META_DESC, CATEGORY_PAGE_TITLE
+}
+
---
+++ src/io/sudheer/practice/simple/AttributeNamesEnumTest.java
@@ -0,0 +1,12 @@
+package io.sudheer.practice.simple;
+
+import io.sudheer.jenkins.utils.AttributeNamesEnum;
+
+public class AttributeNamesEnumTest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println(AttributeNamesEnum.META_KEYWORD.toString());
+	}
+
+}
